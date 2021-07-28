580b1c19991e3340827ad575e73d8ca23a55aae7
Evan Tan <36868915+evan-tan@users.noreply.github.com>
2021-07-29 04:14:14 +0800

    Added lab 1 files, update setup

--- setup/install.sh
+++ setup/install.sh
@@ -7,7 +7,5 @@ conda create --name ece4179 python=3.8 -y && \
 conda activate ece4179
 
 conda install -y pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
-conda install -y numpy matplotlib pandas scipy
 
-pip install detectron2 -f \
-    https://dl.fbaipublicfiles.com/detectron2/wheels/cu110/torch1.7/index.html
+pip install -r requirements.txt
